a62c41ee4cf67f10cd8ebf818a339cb171e627cd
Harvey56 <hevel56@gmail.com>
2018-07-27 13:44:40 +1000

    re-build

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, '/dist'),
-  },
+  //output: {
+  //  filename: 'main.js',
+  //  path: path.resolve(__dirname, '/dist'),
+  //},
   module: {
     rules: [
       {
